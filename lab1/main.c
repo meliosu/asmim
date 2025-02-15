2421a6803cdf75f8faf9feacab94c19c107bd2fc
meliosu <alex.atyurin@gmail.com>
2025-02-15 21:44:56 +0700

    rewrite

--- lab1/main.c
+++ lab1/main.c
@@ -1,30 +1,88 @@
+#include <emmintrin.h>
 #include <immintrin.h>
 #include <stdio.h>
-#include <x86intrin.h>
 
-#define OPERATIONS 200000000
+#define OPERATIONS 60000000
 
 typedef unsigned long long u64;
 
-void run_latency(int ops);
-void run_throughput(int ops);
+static void used(__m128d value) {
+    asm volatile("" : "+xmm"(value));
+}
+
+void warmup() {
+    __m128d value = _mm_set1_pd(2.0);
+
+    for (int i = 0; i < OPERATIONS; i++) {
+        value = _mm_mul_pd(value, value);
+    }
+
+    used(value);
+}
+
+double measure_latency() {
+    __m128d value = _mm_set1_pd(2.0);
+
+    u64 start = __rdtsc();
+
+    for (int i = 0; i < OPERATIONS / 6; i++) {
+        value = _mm_mul_pd(value, value);
+        value = _mm_mul_pd(value, value);
+        value = _mm_mul_pd(value, value);
+        value = _mm_mul_pd(value, value);
+        value = _mm_mul_pd(value, value);
+        value = _mm_mul_pd(value, value);
+    }
+
+    u64 end = __rdtsc();
+
+    used(value);
+
+    return (double)(end - start) / (double)OPERATIONS;
+}
+
+double measure_throughput() {
+    __m128d value0 = _mm_set1_pd(2.0);
+    __m128d value1 = _mm_set1_pd(3.0);
+    __m128d value2 = _mm_set1_pd(4.0);
+    __m128d value3 = _mm_set1_pd(5.0);
+    __m128d value4 = _mm_set1_pd(6.0);
+    __m128d value5 = _mm_set1_pd(7.0);
+
+    asm volatile("");
+
+    u64 start = __rdtsc();
+
+    for (int i = 0; i < OPERATIONS / 6; i++) {
+        value0 = _mm_mul_pd(value0, value0);
+        value1 = _mm_mul_pd(value1, value1);
+        value2 = _mm_mul_pd(value2, value2);
+        value3 = _mm_mul_pd(value3, value3);
+        value4 = _mm_mul_pd(value4, value4);
+        value5 = _mm_mul_pd(value5, value5);
+    }
+
+    u64 end = __rdtsc();
+
+    used(value0);
+    used(value1);
+    used(value2);
+    used(value3);
+    used(value4);
+    used(value5);
+
+    return (double)OPERATIONS / (double)(end - start);
+}
 
 int main() {
-    u64 start, end;
+    warmup();
 
-    start = __rdtsc();
-    run_latency(OPERATIONS);
-    end = __rdtsc();
-    double latency = (double)(end - start) / (double)OPERATIONS;
-
-    start = __rdtsc();
-    run_throughput(OPERATIONS);
-    end = __rdtsc();
-    double throughput = (double)OPERATIONS / (double)(end - start);
+    double latency = measure_latency();
+    double throughput = measure_throughput();
 
     printf(
-        "LATENCY: %.2lf\n"
-        "THROUGHPUT: %.2lf\n",
+        "Latency: %.2lf\n"
+        "Throughput: %.2lf\n",
         latency,
         throughput
     );
